ef1d18e26339ac81e2cf2a2463f6e481cb0fbbaf
fawdlstty <f@fawdlstty.com>
2025-10-26 23:17:18 +0800

    add test

--- faml/src/lib.rs
+++ faml/src/lib.rs
@@ -1,21 +1,8 @@
-// #[cfg(test)]
-// pub mod test;
+#[cfg(test)]
+pub mod test;
 
 pub mod ast;
 mod string_utils;
 
 pub use ast::faml_expr::{FamlExpr, FamlExprImpl};
 pub use ast::faml_value::FamlValue;
-use serde::Deserialize;
-
-pub fn from_str<T: for<'a> Deserialize<'a>>(input: &str) -> anyhow::Result<T> {
-    from_expr(FamlExpr::from_str(input)?)
-}
-
-pub fn from_expr<T: for<'a> Deserialize<'a>>(expr: FamlExpr) -> anyhow::Result<T> {
-    from_value(expr.evalute()?)
-}
-
-pub fn from_value<T: for<'a> Deserialize<'a>>(value: FamlValue) -> anyhow::Result<T> {
-    value.deserialize()
-}
--- faml/src/test.rs
+++ faml/src/test.rs
@@ -1,4 +1,4 @@
-use crate::FamlExprBase;
+use crate::FamlExpr;
 
 #[test]
 fn test1() -> anyhow::Result<()> {
@@ -7,9 +7,51 @@ fn test1() -> anyhow::Result<()> {
 value = 12
 name = $"hello world {value + 12}"
 "#;
-    let mut root = FamlExprBase::from_str(faml_str)?;
+    let mut root = FamlExpr::from_str(faml_str)?;
     root["hello"]["value"].set_int(30);
     let root = root.evalute()?;
     assert_eq!(root["hello"]["name"].as_str(), "hello world 42");
+    Ok(())
+}
+
+#[derive(serde::Deserialize)]
+struct MyStructHello {
+    pub age: i32,
+    pub name: String,
+}
+
+#[derive(serde::Deserialize)]
+struct MyStruct {
+    pub hello: MyStructHello,
+}
+
+#[test]
+fn test2() -> anyhow::Result<()> {
+    let faml_str = r#"
+[hello]
+age = 12
+name = "maria"
+"#;
+    let mut root = FamlExpr::from_str(faml_str)?;
+    root["hello"]["age"].set_int(30);
+    let data: MyStruct = root.deserialize()?;
+    assert_eq!(data.hello.age, 30);
+    assert_eq!(data.hello.name, "maria");
+    Ok(())
+}
+
+#[test]
+fn test3() -> anyhow::Result<()> {
+    let faml_str = r#"
+[hello]
+value1 = 12
+value2 = 13
+
+[hello.test]
+value3 = $"value1[{super.value1}], value2[{base.hello.value2}]"
+"#;
+    let root = FamlExpr::from_str(faml_str)?;
+    let value3 = root["hello"]["test"]["value3"].evalute()?.as_str();
+    assert_eq!(value3, "value1[12], value2[13]");
     Ok(())
 }
